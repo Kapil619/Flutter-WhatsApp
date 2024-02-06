94eba5f2e6a2d2a6fda2c3b718ccf0a786a8b90b
Kapil619 <kapilbadokar321@gmail.com>
2024-02-07 01:17:13 +0530

    Added firebase plugins and configured firebase for the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/screens/mobile_layout_screen.dart';
 import 'package:whatsapp/screens/web_layout_screen.dart';
 import 'utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
