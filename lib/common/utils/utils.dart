fc4238d1c24fb7586dc985ee1d79dbb12ba5dee0
Kapil619 <kapilbadokar321@gmail.com>
2024-02-15 23:28:44 +0530

    Configured imager_picker for ios & Created image picker function.

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackbar(
     {required BuildContext context,
@@ -11,4 +14,18 @@ void showSnackbar(
       backgroundColor: color,
     ),
   );
+}
+
+Future<File?> pickImageFromfGallery(BuildContext context) async {
+  File? image;
+  try {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      image = File(pickedImage.path);
+    }
+  } catch (e) {
+    showSnackbar(context: context, content: e.toString(), color: Colors.red);
+  }
+  return image;
 }
--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,11 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UserInformationScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:whatsapp/common/utils/utils.dart';
+
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromfGallery(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -13,12 +36,38 @@ class UserInformationScreen extends StatelessWidget {
             children: [
               Stack(
                 children: [
-                  const CircleAvatar(
-                    backgroundImage:
-                        NetworkImage('https://via.placeholder.com/151'),
+                  image == null
+                      ? const CircleAvatar(
+                          radius: 64,
+                          backgroundImage:
+                              NetworkImage('https://via.placeholder.com/151'),
+                        )
+                      : CircleAvatar(
+                          radius: 64,
+                          backgroundImage: FileImage(image!),
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                        onPressed: selectImage,
+                        icon: const Icon(Icons.add_a_photo)),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your Name',
+                      ),
+                    ),
                   ),
-                  IconButton(
-                      onPressed: () {}, icon: const Icon(Icons.add_a_photo))
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.done))
                 ],
               )
             ],
