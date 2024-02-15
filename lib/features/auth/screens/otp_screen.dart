855b06929e4ffe6a33f5b0941a00659f716db8a1
Kapil619 <kapilbadokar321@gmail.com>
2024-02-15 21:21:44 +0530

    Created the OTPScreen UI

--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
 
 class OTPScreen extends StatefulWidget {
   static const String routeName = '/otp-screen';
@@ -12,6 +13,33 @@ class OTPScreen extends StatefulWidget {
 class _OTPScreenState extends State<OTPScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: const Text('Verifying your number'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text('We have sent an SMS with a code to your number.'),
+            SizedBox(
+              width: size.width * 0.5,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: '- - - - - -',
+                  hintStyle: TextStyle(fontSize: 30),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (val) {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
