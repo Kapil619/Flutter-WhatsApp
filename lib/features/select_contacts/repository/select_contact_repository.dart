3b5f7d24620b7853e11911556724464966676552
Kapil619 <kapilbadokar321@gmail.com>
2024-02-16 11:26:41 +0530

    Added & configured the Flutter Contacts dependency.

---
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SelectContactRepository {
+  final FirebaseFirestore firestore;
+
+  SelectContactRepository({required this.firestore});
+}
